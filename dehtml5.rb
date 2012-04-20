c8b0af6403b54442f6ff37b35a6238967bef016a
Mislav Marohnić <mislav.marohnic@gmail.com>
2012-04-20 14:13:44 +0200

    improve de-HTML5ization

--- dehtml5.rb
+++ dehtml5.rb
@@ -1,16 +1,16 @@
 require 'nokogiri'
 doc = Nokogiri::HTML ARGF
 
-doc.search('article, section, figure, figcaption').reverse.each do |elem|
-	type = elem.name
-	type = 'caption' if type == 'figcaption'
-	elem.name = 'div'
+doc.search('article, section, figure, figcaption, hgroup, mark').reverse.each do |elem|
+  type = elem.name
+  type = 'caption' if type == 'figcaption'
+  elem.name = type == 'mark' ? 'span' : 'div'
 
-	classnames = elem['class'].to_s.lstrip.split(/\s+/)
-	unless classnames.include? type
-		classnames << type 
-		elem['class'] = classnames.join(' ')
-	end
+  classnames = elem['class'].to_s.lstrip.split(/\s+/)
+  unless classnames.include? type
+    classnames << type 
+    elem['class'] = classnames.join(' ')
+  end
 end
 
 puts doc
